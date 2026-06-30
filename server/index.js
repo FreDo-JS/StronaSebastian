3615081cbe46f6d65d53a304770c1c30e498d6e0
myNameIsJeff <123896394+FreDo-JS@users.noreply.github.com>
2026-06-30 23:00:08 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import 'dotenv/config'
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }))
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://api.juniordevops.xyz' }))
 app.use(express.json())
 
 const transporter = nodemailer.createTransport({
